f04476b6f13d5029817e95efd11b94852f139990
edogola4 <edogola4@gmail.com>
2025-02-25 13:55:12 +0300

    test passing

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,25 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+        self.sorted_word = sorted(word.lower())
+        
+    
+    def match(self, candidates):
+        
+        matches = []
+
+        for candidate in candidates:
+
+            candidate_lower = candidate.lower()
+
+
+            if candidate_lower == self.word.lower():
+                continue
+
+            if sorted(candidate_lower) == self.sorted_word:
+                matches.append(candidate)
+
+        return matches
